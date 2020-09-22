10b28771065d45ae0085221fed20596ef2d57c02
FernandesGuilherme <guilherme.logus@gmail.com>
2020-09-21 21:55:09 -0300

    Atividade de Lógica de Programação

--- src/main/java/Semana6/mainTeste.java
+++ src/main/java/Semana6/mainTeste.java
@@ -3,13 +3,20 @@ package Semana6;
 public class mainTeste {
     public static void main(String[] args) {
         Ator ator = new Ator("Chadwick Boseman");
-        Filme filme = new Filme("Pantera negra", " ", 2019);
+        Filme filme = new Filme("Pantera negra", " Pantera negra", 2019);
         Papel papel = new Papel("Pantera negra", true, ator);
 
         filme.addPapel(ator,true, true);
 
         System.out.println(ator.getNome());
-        System.out.println(ator.getFilme().);
+        System.out.println(filme.getAtores());
+
+        for (Ator atores : filme.getAtores()){
+            System.out.printf(atores.getNome().toUpperCase());
+        }
+        for (Ator papeis : filme.getPapeis()){
+            System.out.printf(" " + papeis.getFilme());
+        }
 
 
     }
